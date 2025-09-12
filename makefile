9070c41b9132221cdcb906b93367df1b6960ce07
Muhammad Bilal <bilal119797@gmail.com>
2025-09-11 20:36:35 -0600

    Modified the test file to be compatible with zksync

--- makefile
+++ makefile
@@ -2,3 +2,7 @@
 foundry-install:;curl -L https://foundry.paradigm.xyz | bash
 
 install:;forge install openzeppelin/openzeppelin-contracts dmfxyz/murky cyfrin/foundry-devops
+
+initialise-zksync:;foundryup-zksync
+
+build-zksync:;forge build --zksync
--- test/MerkleAirdropTest.t.sol
+++ test/MerkleAirdropTest.t.sol
@@ -6,14 +6,15 @@ import {Test, console} from "forge-std/Test.sol";
 import {AppleToken} from "../src/AppleToken.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 import {DeployMerkleAirdrop} from "../script/DeployMerkleAirdrop.s.sol";
+import {ZkSyncChainChecker} from "foundry-devops/src/ZkSyncChainChecker.sol";
 
-contract MerkleAirdropTest is Test {
+contract MerkleAirdropTest is Test, ZkSyncChainChecker {
     AppleToken public appleToken;
     MerkleAirdrop public merkleAirdrop;
     bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    address public gasPayer;
     address user;
     uint256 userPrivateKey;
-    DeployMerkleAirdrop public deployer;
 
     bytes32[] public PROOF = [
         bytes32(0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a),
@@ -23,18 +24,38 @@ contract MerkleAirdropTest is Test {
     uint256 public AMOUNT_TO_SEND = 1000 * 1e18;
 
     function setUp() public {
-        deployer = new DeployMerkleAirdrop();
-        (merkleAirdrop, appleToken) = deployer.deployMerkleAirdrop();
+        if (!isZkSyncChain()) {
+            DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
+            (merkleAirdrop, appleToken) = deployer.deployMerkleAirdrop();
+        } else {
+            appleToken = new AppleToken();
+            merkleAirdrop = new MerkleAirdrop(ROOT, appleToken);
+            appleToken.mint(appleToken.owner(), AMOUNT_TO_SEND);
+            appleToken.transfer(address(merkleAirdrop), AMOUNT_TO_SEND);
+        }
+
         (user, userPrivateKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
+    }
+
+    function signMessage(uint256 privKey, address account) public view returns (uint8 v, bytes32 r, bytes32 s) {
+        bytes32 hashedMessage = merkleAirdrop.getMessageHash(account, AMOUNT_TO_CLAIM);
+        (v, r, s) = vm.sign(privKey, hashedMessage);
     }
 
     function testUserCanClaim() public {
         uint256 startingBalance = appleToken.balanceOf(user);
+
+        // get the signature
         vm.startPrank(user);
-        merkleAirdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        (uint8 v, bytes32 r, bytes32 s) = signMessage(userPrivateKey, user);
         vm.stopPrank();
+
+        // gasPayer claims the airdrop for the user
+        vm.prank(gasPayer);
+        merkleAirdrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
         uint256 endingBalance = appleToken.balanceOf(user);
-        console.log("Ending Balance: ", endingBalance);
+        console.log("Ending balance: %d", endingBalance);
         assertEq(endingBalance - startingBalance, AMOUNT_TO_CLAIM);
     }
 }
